28adc2bbd6ab13e438d56761e53b8582cc901610
Matthew Quint <mattquint111@gmail.com>
2020-10-12 09:47:04 -0500

    initial files

---
+++ main_v2.js
@@ -0,0 +1,5 @@
+// initial constants
+const apiKey = "0310c1a97f001b72c2466fdfc9e4f305";
+const searchMovieUrl = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=`;
+const path_nowPlaying = "/movie/now_playing";
+

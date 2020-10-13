120b448fddeebfc950313d6b696442d17dc1de9a
ChapmanChappelle <chapman.chappelle@gmail.com>
2020-10-13 14:03:01 -0400

    made div go woosh

--- main.js
+++ main.js
@@ -1,92 +1,94 @@
-
 // initial variables
-const apiKey = "0310c1a97f001b72c2466fdfc9e4f305";
-const searchMovieUrl = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=`;
-const nowPlayingUrl = `https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}`;
+const apiKey = "0310c1a97f001b72c2466fdfc9e4f305"
+const searchMovieUrl = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=`
+const nowPlayingUrl = `https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}`
 const popularUrl = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}`
 const topRatedUrl = `https://api.themoviedb.org/3/movie/top_rated?api_key=${apiKey}`
 const upcomingUrl = `https://api.themoviedb.org/3/movie/upcoming?api_key=${apiKey}`
 
-
 // search movies
 const searchInput = document.getElementById("inputValue")
-const submitButton = document.getElementById('submitButton')
+const submitButton = document.getElementById("submitButton")
 
-submitButton.addEventListener('click', function() {
-    const movieName = searchInput.value;
-    const searchUrl = searchMovieUrl + movieName
+/* submitButton.addEventListener("click", function () {
+   const movieName = searchInput.value
+   const searchUrl = searchMovieUrl + movieName
+   createPlaylist(searchUrl, "searched")
+   searchInput.value = ""
+}) */
 
-    createPlaylist(searchUrl, 'searched')
-
-    
-    searchInput.value = ''
+// listener for List Builder button
+let listNameTxt = document.getElementById("listNameTxt")
+let listDescrTxt = document.getElementById("listDescrTxt")
+let submitListInfoBtn = document.getElementById("submitListInfoBtn")
+let listBuilderInfoDiv = document.getElementById("listBuilderInfoDiv")
+let addFilmsDiv = document.getElementById("addFilmsDiv")
+submitListInfoBtn.addEventListener("click", function () {
+   listBuilderInfoDiv.style.display = "none"
+   addFilmsDiv.style.display = "block"
 })
 
+/* // when you search through movies to add them to the playlist
 let addFilmsSearchTxt = document.getElementById("addFilmsSerchTxt")
-
-addFilmsSearchTxt.addEventListener('input', function() {
-    let search = addFilmsSearchTxt.value
-    let searchUrl = searchMovieUrl + search
-    createPlaylist(searchUrl,'searched')
+addFilmsSearchTxt.addEventListener("input", function () {
+   let search = addFilmsSearchTxt.value
+   let searchUrl = searchMovieUrl + search
+   createPlaylist(searchUrl, "searchResult")
 })
-
+ */
 // create homepage playlists
 createPlaylist(nowPlayingUrl, "nowPlaying")
 createPlaylist(popularUrl, "popular")
 createPlaylist(topRatedUrl, "topRated")
 createPlaylist(upcomingUrl, "upcoming")
 
-
 // FUNCTIONS
 function createPlaylist(playlistUrl, playlistName) {
-    fetch(playlistUrl)
-    .then(res => res.json())
-    .then(data => {
-        //array of movie objects
-        const movieArray = data.results
-        
-        const playList = document.getElementById(`${playlistName}List`)
-        const movieObject = document.createElement('div')
-        movieObject.classList.add('movieObject')
+   fetch(playlistUrl)
+      .then((res) => res.json())
+      .then((data) => {
+         //array of movie objects
+         const movieArray = data.results
 
-        for (let i = 0; i < movieArray.length; i++) {
+         const playList = document.getElementById(`${playlistName}List`)
+         const movieObject = document.createElement("div")
+         movieObject.classList.add("movieObject")
+
+         for (let i = 0; i < movieArray.length; i++) {
             let movie = movieArray[i]
             let movieData = `
             <img class="moviePoster" src="https://image.tmdb.org/t/p/w200/${movie.poster_path}" alt="movie poster" id=${movie.id}>
             <span class="movieTitle">${movie.original_title}</span>
             <span class="movieReleaseDate">${movie.release_date}</span>
             `
-                const movieObject = document.createElement('div')
-                movieObject.classList.add('movieObject')
-                movieObject.innerHTML = movieData
-                playList.appendChild(movieObject)
-            }
-
-        })
+            const movieObject = document.createElement("div")
+            movieObject.classList.add("movieObject")
+            movieObject.innerHTML = movieData
+            playList.appendChild(movieObject)
+         }
+      })
 }
-
-
 
 // Event Delegation
 document.onclick = function (event) {
-    const target = event.target;
+   const target = event.target
 
-  
-    if (target.tagName.toLowerCase() === "img") {
-        const movieContent = target.parentElement.parentElement.parentElement.nextElementSibling
-        movieContent.classList.toggle("content-display")
+   if (target.tagName.toLowerCase() === "img") {
+      const movieContent =
+         target.parentElement.parentElement.parentElement.nextElementSibling
+      movieContent.classList.toggle("content-display")
 
-        console.log(target.id)
-        const movieSpotlight = async (playlistUrl, spotlight) => {
-            const response = await (fetch(playlistUrl))
-            const movieArray = await response.json()
-            const resultsArray = movieArray.results
+      console.log(target.id)
+      const movieSpotlight = async (playlistUrl, spotlight) => {
+         const response = await fetch(playlistUrl)
+         const movieArray = await response.json()
+         const resultsArray = movieArray.results
 
-            //filter array for id
-            let specificMovie = resultsArray.filter(item => item.id == target.id)
-            const highlight = document.getElementById(spotlight)
-            
-            highlight.innerHTML = `
+         //filter array for id
+         let specificMovie = resultsArray.filter((item) => item.id == target.id)
+         const highlight = document.getElementById(spotlight)
+
+         highlight.innerHTML = `
             <div class = "movie-spotlight">
                 <div id = "spotlight-title">${specificMovie[0].title}</div>
                 <div>Release date:${specificMovie[0].release_date}</div>
@@ -94,17 +96,16 @@ document.onclick = function (event) {
                 <div>Ratings: ${specificMovie[0].vote_average} in ${specificMovie[0].vote_count} votes</div>
             </div>
             `
-        
-            console.log(resultsArray)
-        
-        }
-        movieSpotlight(nowPlayingUrl, "nowPlayingContent")
-        movieSpotlight(popularUrl, 'popularContent')
-        movieSpotlight(topRatedUrl, 'topRatedContent')
-        movieSpotlight(upcomingUrl, 'upcomingContent')
 
-        //   const section = target.parentElement;
-        //   const content = section.nextElementSibling;
-        //   content.classList.add("content-display");
-    }
+         console.log(resultsArray)
+      }
+      movieSpotlight(nowPlayingUrl, "nowPlayingContent")
+      movieSpotlight(popularUrl, "popularContent")
+      movieSpotlight(topRatedUrl, "topRatedContent")
+      movieSpotlight(upcomingUrl, "upcomingContent")
+
+      //   const section = target.parentElement;
+      //   const content = section.nextElementSibling;
+      //   content.classList.add("content-display");
+   }
 }

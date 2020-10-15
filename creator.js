f6f619f59c2d781f86053398d29dfb4834357e69
ChapmanChappelle <chapman.chappelle@gmail.com>
2020-10-15 17:45:01 -0400

    fixed the problem where the movies weren't adding to a list

--- creator.js
+++ creator.js
@@ -1,9 +1,8 @@
 import { signIn, signUp } from "./authentication/auth.js"
-firebase.auth().onAuthStateChanged(function(user) {
+firebase.auth().onAuthStateChanged(function (user) {
    if (user) {
       btn.style.display("none")
-   }
-   else {
+   } else {
       logout.style.display("none")
    }
 })
@@ -25,80 +24,71 @@ var listName
 var tempArr = []
 
 // first window - gets list name from user
-if (submitListInfoBtn) {
-   submitListInfoBtn.addEventListener("click", function () {
-      listBuilderInfoDiv.style.display = "none"
-      addFilmsDiv.style.display = "flex"
-      listName = listNameTxt.value
-      // creates an empty collection and doc to hold the list
-      firebase.auth().onAuthStateChanged(function (user) {
-         if (user) {
-            db.collection("users")
-               .doc(user.uid)
-               .collection("playlists")
-               .doc(listName)
-               .set({})
-         }
-      })
+submitListInfoBtn.addEventListener("click", function () {
+   listBuilderInfoDiv.style.display = "none"
+   addFilmsDiv.style.display = "flex"
+   listName = listNameTxt.value
+   firebase.auth().onAuthStateChanged(function (user) {
+      if (user) {
+         db.collection("users")
+            .doc(user.uid)
+            .collection("playlists")
+            .doc(listName)
+            .set({})
+      }
    })
-}
+})
 
 // search button
-if (addFilmsSearchBtn) {
-   addFilmsSearchBtn.addEventListener("click", function () {
-      searchResultsDiv.innerHTML = ""
-      let userInput = addFilmsSearchTxt.value
-      let searchUrl = searchMovieUrl + userInput
-      // print list of movies
-      fetch(searchUrl)
-         .then((res) => res.json())
-         .then((data) => {
-            let movieArray = data.results
-            console.log(movieArray)
-            for (let i = 0; i < movieArray.length; i++) {
-               let movie = movieArray[i]
-               let movieData = `
+addFilmsSearchBtn.addEventListener("click", function () {
+   searchResultsDiv.innerHTML = ""
+   let userInput = addFilmsSearchTxt.value
+   let searchUrl = searchMovieUrl + userInput
+   // print list of movies
+   fetch(searchUrl)
+      .then((res) => res.json())
+      .then((data) => {
+         let movieArray = data.results
+         console.log(movieArray)
+         for (let i = 0; i < movieArray.length; i++) {
+            let movie = movieArray[i]
+            let movieObject = `
                 <img class="addMoviePoster" src="https://image.tmdb.org/t/p/w200/${movie.poster_path}" alt="movie poster" id=${movie.id}>`
-               let movieObject = document.createElement("div")
-               movieObject.classList.add("movieObject")
-               movieObject.innerHTML = movieData
-               searchResultsDiv.appendChild(movieObject)
-            }
-         })
-   })
-}
+            let movieObjectDiv = `<div id="test">${movieObject}</div>`
+            searchResultsDiv.insertAdjacentHTML("beforeend", movieObjectDiv)
+         }
+      })
+})
 
 // fetches movies added to tempArr and puts them inside the collection under listName
-if (addFilmsToListBtn) {
-   addFilmsToListBtn.addEventListener("click", function () {
-      for (let i = 0; i < tempArr.length; i++) {
-         fetch(
-            `https://api.themoviedb.org/3/movie/${tempArr[i]}?api_key=0310c1a97f001b72c2466fdfc9e4f305`
-         )
-            .then((res) => res.json())
-            .then((data) => {
-               firebase.auth().onAuthStateChanged(function (user) {
-                  if (user) {
-                     let userId = user.uid
-                     db.collection("users")
-                        .doc(userId)
-                        .collection("playlists")
-                        .doc(listName)
-                        .update({
-                           playlist: firebase.firestore.FieldValue.arrayUnion(
-                              data
-                           ),
-                        })
-                        .catch((e) => {
-                           console.log(e)
-                        })
-                  }
-               })
+addFilmsToListBtn.addEventListener("click", function () {
+   console.log(listName)
+   for (let i = 0; i < tempArr.length; i++) {
+      fetch(
+         `https://api.themoviedb.org/3/movie/${tempArr[i]}?api_key=0310c1a97f001b72c2466fdfc9e4f305`
+      )
+         .then((res) => res.json())
+         .then((data) => {
+            firebase.auth().onAuthStateChanged(function (user) {
+               if (user) {
+                  let userId = user.uid
+                  db.collection("users")
+                     .doc(userId)
+                     .collection("playlists")
+                     .doc(listName)
+                     .update({
+                        playlist: firebase.firestore.FieldValue.arrayUnion(
+                           data
+                        ),
+                     })
+                     .catch((e) => {
+                        console.log(e)
+                     })
+               }
             })
-      }
-      window.location.href = "lists.html"
-   })
-}
+         })
+   }
+})
 
 //--------------Login modal-------------------------
 var modal = document.getElementById("myModal")
